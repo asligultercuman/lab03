68c65755e6eddc6edfe58682ad6ba46c88e0d5be
Asligul Tercuman <asligultercumann@gmail.com>
2024-10-26 15:47:58 +0300

    ilk fonksiyon için enum kısmı eklendi

--- Expression.cpp
+++ Expression.cpp
@@ -4,12 +4,86 @@
 #include <cctype>
 using namespace std;
 
+// Enum ile operator onceligi belirleme
+enum class Precedence {
+	LOW = 1,
+	HIGH = 2,
+	MEDIUM  =3,
+	NONE = 0
+};
+
+Precedence getPrecedence(char op) {
+	switch (op) {
+	case '+':
+	case '-':
+		return Precedence::LOW;
+	case '*':
+	case '/':
+		return Precedence::MEDIUM;
+	case '(':
+	case ')':
+		return Precedence::HIGH;
+	default:
+		return Precedence::NONE;
+	}
+}
+
 ///----------------------------------------------------------------------------------
 /// Given an expression in infix-notation, converts it to a string in post-fix notation 
 /// 
-string Infix2Postfix(string &s) {
-	// Fill this in
-	string result;
+string Infix2Postfix(string& s) {
+	stack<char> op;
+	string result, toNoneSpace;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] != ' ') { toNoneSpace.push_back(s[i]); }
+	}
+	s = toNoneSpace;
+	char tmp = '0';
+	for (char c : s)
+	{
+		if (c >= '0' && c <= '9')
+		{
+			if (tmp < '0' || tmp > '9')
+			{
+				result.push_back(' ');
+				result.push_back(c);
+			}
+			else result.push_back(c);
+		}
+		else if (c == '(') { op.push('('); }
+		else if (c == ')')
+		{
+			while (op.top() != '(')
+			{
+				result.push_back(' ');
+				result.push_back(op.top());
+				op.pop();
+			}
+			op.pop();
+		}
+		else
+		{
+			while (!op.empty()) {
+				if (getPrecedence(c) <= getPrecedence(op.top()) && op.top() != '(')
+				{
+					result.push_back(' ');
+					result.push_back(op.top());
+					op.pop();
+				}
+				else break;
+			}
+			op.push(c);
+		}
+		tmp = c;
+	}
+	//Kalan elemanlari ekleme islemi
+	while (!op.empty())
+	{
+		result.push_back(' ');
+		result.push_back(op.top());
+		op.pop();
+	}
 
 	return result;
 } // end-Infix2Postfix
